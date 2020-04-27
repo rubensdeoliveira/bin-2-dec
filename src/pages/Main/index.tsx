835069d5aca0f56ee0e23b141c83919557e97594
rubensojunior <rubensojunior6@gmail.com>
2020-04-27 17:47:22 -0300

    implementing input events

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useRef } from 'react'
 
 import { GoFileBinary } from 'react-icons/go'
-import { TiSortNumerically } from 'react-icons/ti'
+import { TiSortNumerically, TiMediaFastForward } from 'react-icons/ti'
 
 import { Container } from './styles'
 
@@ -9,22 +9,62 @@ import Input from '../../components/Input'
 
 const Main: React.FC = () => {
   const [decimal, setDecimal] = useState('')
-  const [hasError, setHasError] = useState(false)
+  const [binary, setBinary] = useState('')
+  const [hasError] = useState(false)
+
   const handleDecimalInputChange = useCallback((event) => {
-    setDecimal(event.target.value)
+    let decimalValue = event.target.value
+
+    setDecimal(decimalValue)
+
+    const binaryArray = []
+
+    while (decimalValue >= 1) {
+      binaryArray.push(decimalValue % 2)
+      decimalValue = Math.floor(decimalValue / 2)
+    }
+
+    const binaryValue = binaryArray.reverse().join('')
+
+    setBinary(binaryValue)
+  }, [])
+
+  const handleBinaryInputChange = useCallback((event) => {
+    const binaryValue = event.target.value
+
+    setBinary(binaryValue)
+
+    let decimalValue = 0
+    let aux = binaryValue.length - 1
+
+    for (let i = 0; i < binaryValue.length; i += 1) {
+      const pow = 2 ** i
+      const result = binaryValue[aux] * pow
+      aux -= 1
+      decimalValue += result
+    }
+
+    setDecimal(decimalValue.toString())
   }, [])
 
   return (
     <Container>
       <Input
         onChange={(event) => handleDecimalInputChange(event)}
-        placeholder="Valor Decimal"
-        name="decimal"
+        placeholder="Valor em decimal"
+        type="Number"
         icon={TiSortNumerically}
         hasError={hasError}
         value={decimal}
       />
-      <Input name="binary" icon={GoFileBinary} hasError={hasError} />
+      <Input
+        onChange={(event) => handleBinaryInputChange(event)}
+        placeholder="Valor dem binário"
+        type="Number"
+        icon={GoFileBinary}
+        hasError={hasError}
+        value={binary}
+      />
     </Container>
   )
 }
